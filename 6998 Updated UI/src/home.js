5d9deeab7a2a91704894929d30b95198d5e94f4f
Tian Liu <8xiaolong8@gmail.com>
2023-04-09 00:47:17 -0700

    Finished remaining search results

--- 6998 Updated UI/src/home.js
+++ 6998 Updated UI/src/home.js
@@ -1,8 +1,19 @@
+// $('#search').keypress(function (e) {
+//     if (e.which == 13) {
+//         search_text = $("input").val()
+//         if ($.trim(search_text)) {
+//             window.location = "/search_results?text=" + search_text
+//         }
+//         return false;
+//     }
+// });
+
+
 $("#search").submit(function(event) {        
     event.preventDefault();
     search_text = $("input").val()
 
     if ($.trim(search_text)) {
-        window.location = "/search_results?text=" + search_text
+        window.location = "/search_results.html#" + search_text
     }
 });
