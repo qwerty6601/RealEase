9c783eaa75e6475a333e1a02840815b70e2fe3e3
Tian Liu <8xiaolong8@gmail.com>
2023-04-09 01:34:22 -0700

    Implemented rating change color based on score

--- 6998 Updated UI/src/search.js
+++ 6998 Updated UI/src/search.js
@@ -1,54 +1,80 @@
 $(document).ready(function() {
     let search_text = location.hash.substring(1); 
-    console.log(search_text)
-    
     search_results = [
         {
             "id": 1,
             "image": "./img/house-1.png",
             "price": "$275,000",
-            "address": "347 East 4th Street, NYC, NY 10009"
+            "unit": 1,
+            "area": "1,164",
+            "address": "347 East 4th Street, NYC, NY 10009",
+            "status": "./img/like.png", 
+            "rating": 8.4,
+            "estimate": "$250,500"
         },
         {
             "id": 2,
             "image": "./img/house-2.png",
             "price": "$459,900",
+            "unit": 3,
+            "area": "2,101",
             "address": "627 East 6th Street, NYC, NY 10009",
+            "status": "./img/dislike.png", 
+            "rating": 3.1,
+            "estimate": "$460,000"
         },
         {
             "id": 3,
             "image": "./img/house-3.png",
             "price": "$489,900",
-            "address": "512 East 11th Street, 5D, NYC, NY 10075"
+            "unit": 4,
+            "area": "2,005",
+            "address": "512 East 11th Street, 5D, NYC, NY 10075",
+            "status": "./img/like.png", 
+            "rating": 5.3,
+            "estimate": "$520,000"
         }
     ]
-    display_search_items(search_results, search_text)
+    show_house_results(search_results, search_text)
 })
 
-// display search items
-function display_search_items(search_results, search_text) {
+// show houses based on search query
+function show_house_results(search_results, search_text) {
     $.each(search_results, function(index, value) {
         $("#search-title").text('Search Results for "' + search_text + '"')
-
         if (value.id == 0) {
             $("#search-houses").append("<div>" + value.title + "</div>")
         }
         else {
             $("#search-houses").append('<div class="col-md-4 house"> \
-                                        <img class="house-img" src="./img/house-1.png"> \
-                                        <span class="house-price">$275,000</span> \
-                                        <span class="house-prop">1 unit(s) | 1,164 sq ft</span> \
-                                        <span class="house-address">347 East 4th Street, NYC, NY 10009</span> \
-                                        <img class="house-fav" src="./img/like.png"> \
-                                        <div class="house-rating">8.4</div> \
-                                        <div class="house-estimate">Estimate: <b>$250,500</b></span></div>')
-
-            
-            // $("#search-houses").append("<div class='col-4 house'>\
-            //                            <a href='/house/" + value.id + "' class='search-links'>\
-            //                            <img src=" + value.image + " class='search-imgs'></a><br>\
-            //                            <div class='search-prices'>" + value.price + "</div>\
-            //                            <p>" + value.address + "</p></div>")
+                                        <a href="/house/' + value.id + '" class="search-links"> \
+                                        <img class="house-img" src="' + value.image + '" class="search-imgs"></a><br> \
+                                        <span class="house-price">' + value.price + '</span> \
+                                        <span class="house-prop">' + value.unit + ' unit(s) | ' + value.area + ' sq ft</span> \
+                                        <span class="house-address">' + value.address + '</span> \
+                                        <input class="house-fav" type="image" src="' + value.status + '"/> \
+                                        <div class="house-rating" id="rating-' + value.id + '">' + value.rating + '</div> \
+                                        <div class="house-estimate">Estimate: <b>' + value.estimate + '</b></span></div>')
+            $("#rating-" + value.id).css("background-color", get_rating_color(value.rating));
         }  
 	});
+}
+
+// get rating color based on ratings score
+function get_rating_color(rating) {
+    if (rating >= 8.0) {
+        return "#0b8c1c";
+    } 
+    else if (rating >= 6.0) {
+        return "#37ad2f";
+    }
+    else if (rating >= 4.0) {
+        return "#7bb541";
+    }
+    else if (rating >= 2.0) {
+        return "#ccc845"
+    }
+    else {
+        return "#c75046"
+    }
 }
