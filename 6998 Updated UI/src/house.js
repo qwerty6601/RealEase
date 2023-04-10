56fecb1cde486f5e8afd8a107676ade43d4c2b50
Tian Liu <8xiaolong8@gmail.com>
2023-04-09 17:29:27 -0700

    Finished implementing house detail template

--- 6998 Updated UI/src/house.js
+++ 6998 Updated UI/src/house.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
     console.log(temp)
 })
 
+// change favorite status
+$('#fav-3').click(function() {
+    // TODO: send http request to backend to update user's like status
+    if ($('#fav-3').attr("src") == "./img/like.png") {
+        $('#fav-3').attr("src", "./img/dislike.png");
+    } else {
+        $('#fav-3').attr("src", "./img/like.png");
+    }
+});
+
 function change_house_img(img) {
     let detail_img = document.getElementById("house-detail-img");
     detail_img.src = img.src;
